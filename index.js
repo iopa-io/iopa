f0624d73ef941be4bc5f1943be092cb93a0f9319
@guycreate <guy@domabo.com>
2015-09-16 09:54:13 -0500

    copyright

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Limerun Project Contributors
+ * Copyright (c) 2015 Internet of Protocols Alliance (IOPA)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
